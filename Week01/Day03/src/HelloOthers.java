a9ec1fe58d667a9a71e0ef2ed9ab4717db7b880c
Zsofia Huszar <huszarevia@gmail.com>
2018-08-02 13:15:10 +0200

    Visszaírtam, hogy Others és befejeztem a feladatot.

--- Week01/Day03/src/HelloOthers.java
+++ Week01/Day03/src/HelloOthers.java
@@ -1,5 +1,9 @@
-public class Hello {
-    public static void main
+public class HelloOthers {
+    public static void main(String [] args) {
+    System.out.println ("Hello, Krisztián!");
+    System.out.println("Hello, Csilla!");
+    System.out.println("Hello Ádám!");
+    }
 
 }
 
